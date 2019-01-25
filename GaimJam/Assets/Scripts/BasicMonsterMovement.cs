04eafa6400c585c023c3e5cb16a831dcabafe611
Vytautas Žilinas <vytautaszilinas@gmail.com>
2019-01-26 00:07:44 +0200

    Added monster which follows stuff

--- GaimJam/Assets/Scripts/BasicMonsterMovement.cs
+++ GaimJam/Assets/Scripts/BasicMonsterMovement.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class BasicMonsterMovement : MonoBehaviour
 {
+    public Rigidbody2D rigidbody;
+    public Transform targetToFollow;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +15,14 @@ public class BasicMonsterMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        FollowTargetWithRotation(targetToFollow, 1, 0.2f);
+    }
+    void FollowTargetWithRotation(Transform target, float distanceToStop, float speed)
+    {
+        if (Vector3.Distance(transform.position, target.position) > distanceToStop)
+        {
+            var direction = target.position - transform.position;
+            rigidbody.AddRelativeForce(direction * speed, ForceMode2D.Force);
+        }
     }
 }
