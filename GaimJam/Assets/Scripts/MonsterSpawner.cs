2e6e7220360568f38d09cfa033508b0d726fe1be
Mantas <man.petrikas@gmail.com>
2019-01-25 21:47:35 +0200

    added folders and spawn script

---
+++ GaimJam/Assets/Scripts/MonsterSpawner.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonsterSpawner : MonoBehaviour
+{
+    public GameObject enemy;                // The enemy prefab to be spawned.
+    public float spawnTime = 3f;            // How long between each spawn.
+    public Transform[] spawnPoints;         // An array of the spawn points this enemy can spawn from.
+
+    void Start()
+    {
+        InvokeRepeating("Spawn", spawnTime, spawnTime);
+    }
+
+// Update is called once per frame
+    void Update()
+    {
+        int spawnPointIndex = Random.Range(0, spawnPoints.Length);
+ 
+        // Create an instance of the enemy prefab at the randomly selected spawn point's position and rotation.
+        Instantiate(enemy, spawnPoints[spawnPointIndex].position, spawnPoints[spawnPointIndex].rotation);
+    }
+}
