921c326cf3d3b2e6aa905ba7b1e7f19d656ae4b2
Vytautas Žilinas <vytautaszilinas@gmail.com>
2019-01-26 14:10:36 +0200

    Added Fighting logic

---
+++ GaimJam/Assets/Scripts/FightingController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class FightingController : MonoBehaviour
+    {
+        public Animator fightingAnimator;
+        public bool DoneFighting;
+        public Vector2 initialFriendlyVelocity;
+        public Vector2 initialEnemyVector;
+        void Start()
+        {
+            
+        }
+        void Update()
+        {
+            
+        }
+        private void OnCollisionEnter2D(Collision2D collision)
+        {
+            if(gameObject.tag == "Enemy")
+            {
+                initialEnemyVector = collision.gameObject.GetComponent<Rigidbody2D>().velocity;
+                initialFriendlyVelocity = gameObject.GetComponent<Rigidbody2D>().velocity;
+
+                gameObject.GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+                collision.gameObject.GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+
+                StartCoroutine(FightingAnimationStart(collision.gameObject));
+            }
+        }
+        public IEnumerator FightingAnimationStart(GameObject enemy)
+        {
+            fightingAnimator.StartPlayback();
+            yield return new WaitForSeconds(3f);
+            fightingAnimator.StopPlayback();
+            gameObject.GetComponent<FriendlyController>().RecalculateHealthAndDirection(initialFriendlyVelocity);
+            enemy.GetComponent<EnemyController>().RecalculateHealthAndDirection(initialFriendlyVelocity);
+        }
+    }
+}
--- GaimJam/Assets/Scripts/FirstLevelScript.cs
+++ GaimJam/Assets/Scripts/FirstLevelScript.cs
@@ -1,5 +1,4 @@
-﻿using Assets.Scripts.Models;
-using System;
+﻿using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
