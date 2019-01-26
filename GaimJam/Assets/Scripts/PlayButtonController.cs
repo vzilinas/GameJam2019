66447a139a3aa303d7eee1d3fc6b6bb0fa04f641
Emilijus Stankus <emilijus@gmail.com>
2019-01-26 23:02:51 +0200

    Added main menu with play button

---
+++ GaimJam/Assets/Scripts/PlayButtonController.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayButtonController : MonoBehaviour
+{
+    public void StartGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
